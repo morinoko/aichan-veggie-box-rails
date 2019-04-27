f9605b8c80a22200aec2210f2554efe85cc4ca8f
Felice Forby <forby.1@gmail.com>
2019-04-27 15:09:38 +0900

    add test for home page

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get home" do
+  	get root_path
+  	assert_response :success
+  end
 end
