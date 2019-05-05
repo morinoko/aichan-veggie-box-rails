cd2841555761f2e0d9c275baabb0d9f446cdffe7
Felice Forby <forby.1@gmail.com>
2019-05-05 10:08:26 +0900

    add menu button (still static)

---
+++ app/javascript/home.js
@@ -0,0 +1,7 @@
+const menuButton = document.getElementById("menu-button");
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+	const menu = document.getElementById("menu");
+	console.log("clicked");
+}
