ded13423dd68d70122052974a3ec4666e5f48205
Felice Forby <forby.1@gmail.com>
2019-05-06 13:59:50 +0900

    add tests for locales

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,4 +5,18 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   	get root_path
   	assert_response :success
   end
+
+  test "should get home for each locale" do
+  	get root_path(locale: :en)
+  	assert_response :success
+  	get root_path(locale: :ja)
+  	assert_response :success
+  end
+
+  test "locales should show proper translations" do
+  	get root_path(locale: :en)
+  	assert_select "h1", "Ai-chan’s Veggie Box"
+  	get root_path(locale: :ja)
+  	assert_select "h1", "あいちゃんのVeggie Box"
+  end
 end
