18ca1a2fbceaf35b4f1a9cea9f08941d79993b2c
Felice Forby <forby.1@gmail.com>
2019-05-08 08:50:52 +0900

    add more tests for email validation

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -21,6 +21,33 @@ class OrderTest < ActiveSupport::TestCase
   	assert_not @order.valid?
   end
 
+  test "email validation should accept valid addresses" do
+  	valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+  												first.last@foo.jp alice+bob@baz.cn]
+
+  	valid_addresses.each do |valid_address|
+  		@order.email = valid_address
+  		assert @order.valid?, "#{valid_address.inspect} should be valid"
+  	end
+  end
+
+  test "email validation should reject invalid addresses" do
+  	invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+
+    invalid_addresses.each do |invalid_address|
+    	@order.email = invalid_address
+    	assert_not @order.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be saved as lower-case" do
+  	mixed_case_email = "UseR@ExaMple.CoM"
+  	@order.email = mixed_case_email
+  	@order.save
+  	assert_equal mixed_case_email.downcase, @order.reload.email
+  end
+
   test "address should be present" do
   	@order.address = ""
   	assert_not @order.valid?
