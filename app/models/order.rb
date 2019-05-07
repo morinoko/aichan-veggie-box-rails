cfefd4905db4669fd0ac17b078503038d67306f4
Felice Forby <forby.1@gmail.com>
2019-05-08 08:51:11 +0900

    validate order email for downcase and valid format

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,9 @@
 class Order < ApplicationRecord
+	before_save { email.downcase! }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :name, presence: true
-	validates :email, presence: true
+	validates :email, presence: true,
+										format: { with: VALID_EMAIL_REGEX }
 	validates :address, presence: true
 	validates :phone, presence: true
 	validates :message, presence: true
