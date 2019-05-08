0ee5a358479e18a3a70758dec6835d97aabaee8f
Felice Forby <forby.1@gmail.com>
2019-05-09 06:45:37 +0900

    set up basic create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,17 @@
 class OrdersController < ApplicationController
+	def create
+		@order = Order.new(order_params)
+
+		if @order.save
+			redirect_to root_path
+		else
+			render 'home/index'
+		end
+	end
+
+	private
+
+	def order_params
+		params.require(:order).permit(:name, :email, :address, :phone, :message)
+	end
 end
