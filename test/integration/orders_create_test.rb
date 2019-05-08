4caae527292c053ec82434e0b99fd69500effdd9
Felice Forby <forby.1@gmail.com>
2019-05-09 08:38:52 +0900

    add test for successful order

--- test/integration/orders_create_test.rb
+++ test/integration/orders_create_test.rb
@@ -12,7 +12,23 @@ class OrdersCreateTest < ActionDispatch::IntegrationTest
     end
 
     assert_template 'home/index'
+    assert_select 'div.alert'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
+  end
+
+  test "submitting valid form should create a new order" do
+    get root_path
+    assert_difference 'Order.count', 1 do
+        post orders_path, params: { order: { name: "Joe Veggie",
+                                             email: "email@example.com",
+                                             address: "1-2-3 Chuo, Tokyo, 300-1111",
+                                             phone: "09-9999-0000",
+                                             message: "Please place my order!" } }
+    end
+
+    follow_redirect!
+    assert_template 'home/index'
+    assert_select 'div.alert-success'
   end
 end
