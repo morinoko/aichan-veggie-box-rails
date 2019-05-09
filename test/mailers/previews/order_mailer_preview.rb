5899dd4da1ff74d43fe7e1e905cd211e7d00388f
Felice Forby <forby.1@gmail.com>
2019-05-10 08:55:13 +0900

    set up mailer preview

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
+	def new_order_email
+		order = Order.new(name: "Joe Smith", email: "joe@gmail.com", address: "1-2-3 Chuo, Tokyo, 333-0000", phone: "090-7777-8888", message: "I want to place an order!")
 
+		OrderMailer.with(order: order).new_order_email
+	end
 end
