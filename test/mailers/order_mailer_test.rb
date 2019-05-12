5a45661241a8a90014e7199d06dbbfaa228b27e6
Felice Forby <forby.1@gmail.com>
2019-05-13 07:08:08 +0900

    add simple test for order mailer

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class OrderMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "new order email" do
+    order = Order.new(name: "Joe Smith", email: "joe@gmail.com",
+    									address: "1-2-3 Chuo, Tokyo, 333-0000", phone: "090-7777-8888",
+    									message: "I want to place an order!")
+
+		email = OrderMailer.with(order: order).new_order_email
+
+		assert_emails 1 do
+			email.deliver_now
+		end
+
+		assert_equal ['aichannoveggiebox@gmail.com'], email.from
+		assert_equal ['aichannoveggiebox@gmail.com'], email.to
+		assert_equal "あいちゃんのベジボックスから新しいメッセージがきました！", email.subject
+  end
 end
