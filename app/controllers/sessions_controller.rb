8e9d7cf1096f4a26e4316731817b2c111bb71248
Felice Forby <forby.1@gmail.com>
2019-05-14 07:08:51 +0900

    set up sessions controller and handle invalid login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+
+  	if user && user.authenticate(params[:session][:password])
+  		flash[:success] = t('sessions.success')
+  		# login in and render root
+  	else
+  		flash.now[:error] = t('sessions.error')
+  		render :new
+  	end
   end
 
   def destroy
