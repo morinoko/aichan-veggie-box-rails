08563d560ac792a7609c74a861089183b6838bce
Felice Forby <forby.1@gmail.com>
2019-05-15 06:47:56 +0900

    update mailer config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,9 +33,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
