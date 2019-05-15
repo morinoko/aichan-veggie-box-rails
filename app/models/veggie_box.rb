a6da2404dbbafaecb2ab583346d2b94963a9dc5c
Felice Forby <forby.1@gmail.com>
2019-05-16 07:47:59 +0900

    add veggie box model validations

--- app/models/veggie_box.rb
+++ app/models/veggie_box.rb
@@ -1,3 +1,14 @@
 class VeggieBox < ApplicationRecord
+	validates :title, presence: true
+	validates :description, presence: true
+	validate :only_one
 	has_one_attached :photo
+
+	private
+
+	def only_one
+		if VeggieBox.count >= 1
+			errors.add :base, 'There can only be one Veggie Box'
+		end
+	end
 end
