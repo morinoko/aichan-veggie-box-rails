3c74d30f730145fa593eb444d1446f9da8b0e169
Felice Forby <forby.1@gmail.com>
2019-05-16 08:16:57 +0900

    add veggie box model tests

--- test/models/veggie_box_test.rb
+++ test/models/veggie_box_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class VeggieBoxTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+    @box = VeggieBox.new(title: "Box", description: "Lots of veggies!")
+ 	end
+
+  test "should be valid" do
+    assert @box.valid?
+  end
+
+  test "title should be present" do
+  	@box.title = ""
+  	assert_not @box.valid?
+  end
+
+  test "description should be present" do
+  	@box.description = ""
+  	assert_not @box.valid?
+  end
+
+  test "should not be more than one veggie box" do
+  	@box.save
+  	box_2 = VeggieBox.new(title: "Box 2", description: "invalid box")
+  	assert_not box_2.valid?
+  end
 end
