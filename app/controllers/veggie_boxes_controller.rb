1305a515fd9dacb09aec8458a53919dd44535e74
Felice Forby <forby.1@gmail.com>
2019-05-16 09:14:09 +0900

    initial set up of veggie boxes controller

--- app/controllers/veggie_boxes_controller.rb
+++ app/controllers/veggie_boxes_controller.rb
@@ -1,4 +1,25 @@
 class VeggieBoxesController < ApplicationController
+	before_action :set_veggie_box
+
   def edit
+  end
+
+  def update
+  	if @veggie_box.update(veggie_box_params)
+  		redirect_to root_path
+  	else
+  		flash.now[:error] = "error!"
+  		render :edit
+  	end
+  end
+
+  private
+
+  def veggie_box_params
+  	params.require(:veggie_box).permit(:title_ja, :title_en, :description_ja, :description_en, :photo)
+  end
+
+  def set_veggie_box
+  	@veggie_box = VeggieBox.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'veggie_boxes/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope "(:locale)", locale: /en|ja/ do
   	root 'home#index'
   	resources :orders, only: %w{create}
   	get 'orders', action: :index, controller: :home
+    resources :veggie_boxes, only: %w{edit update}
 
   	# Login/Logout
   	get '/login', to: 'sessions#new'
