95290190320918fde0a90398c6274f28da856e2f
Felice Forby <forby.1@gmail.com>
2019-05-19 15:34:37 +0900

    update domain in mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'aichan-veggie-box.herokuapp.com'
+  host = 'aichannoveggiebox.com'
   config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
