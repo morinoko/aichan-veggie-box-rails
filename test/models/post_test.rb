d25a1aec086d00835caa7f85d6180955b64038f1
Felice Forby <forby.1@gmail.com>
2019-05-20 06:55:40 +0900

    add post model tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@post = posts(:one)
+	end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "title should be present" do
+  	@post.title = ""
+  	assert_not @post.valid?
+  end
+
+  test "content should be present" do
+  	@post.content = ""
+  	assert_not @post.valid?
+  end
 end
