4778cc2bace4bd91c46d4dba8647d84b4bb4fde6
Felice Forby <forby.1@gmail.com>
2019-05-20 08:10:07 +0900

    remove unneeded content field from Post model

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,6 @@ ActiveRecord::Schema.define(version: 2019_05_19_213732) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
