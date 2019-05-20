a9c8cb3949a2e2ea9a50399c0ffcb96711e0f44b
Felice Forby <forby.1@gmail.com>
2019-05-21 08:29:30 +0900

    fix post test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -3,11 +3,13 @@ require 'test_helper'
 class PostTest < ActiveSupport::TestCase
 	def setup
 		@post = posts(:one)
+    @post.content = action_text_rich_texts(:one)
 	end
 
   test "should be valid" do
-  	# doesn't work yet
     assert @post.valid?
+    # check content is actually present
+    assert_match "<p>In\ a\ <i>million<\/i>\ stars!<\/p>", @post.content.body.to_s
   end
 
   test "title should be present" do
