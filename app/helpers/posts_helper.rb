7146805c2a5f22ec5e2e5296b53c90231a796940
Felice Forby <forby.1@gmail.com>
2019-05-25 09:02:59 +0900

    show errors for posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,12 @@
 module PostsHelper
 	include Pagy::Frontend
+
+	def attribute_errors(object, attribute)
+		if object.errors.full_messages_for(attribute.to_sym).any?
+			object.errors.full_messages_for(attribute).each do |message| 
+				message = "<p class=\"error\">#{message}</p>"
+				return message.html_safe
+			end
+		end
+	end
 end
