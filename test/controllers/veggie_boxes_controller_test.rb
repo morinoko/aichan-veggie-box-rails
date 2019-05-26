023cfd479a28e5a6c31c0a4b04d3889f947320c5
Felice Forby <forby.1@gmail.com>
2019-05-27 07:46:51 +0900

    fix/add tests for veggie boxes controller

--- test/controllers/veggie_boxes_controller_test.rb
+++ test/controllers/veggie_boxes_controller_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class VeggieBoxesControllerTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@user = users(:joe)
+		post login_path, params: { session: { email: @user.email, password: 'password' } }
+    @box = veggie_boxes(:box_one)
+ 	end
+
+	test "should get new" do
+		@new_box = VeggieBox.new
+		get new_veggie_box_url(@new_box)
+		assert_response :success
+	end
+
   test "should get edit" do
-    get veggie_boxes_edit_url
+    get edit_veggie_box_url(@box.id)
     assert_response :success
   end
 
