aa6717429c0b5ec4e5e8627672d8314a9d8884f5
Felice Forby <forby.1@gmail.com>
2019-06-05 07:26:56 +0900

    add helper methods for recaptcha

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
+require 'net/http'
+
 class ApplicationController < ActionController::Base
 	include SessionsHelper
 	before_action :set_locale
+
+	RECAPTCHA_MINIMUM_SCORE = 0.5
 
 	def set_locale
 		I18n.locale = params[:locale] || I18n.default_locale
@@ -9,4 +13,13 @@ class ApplicationController < ActionController::Base
 	def default_url_options
 		{ locale: I18n.locale }
 	end
+
+	def verify_recaptcha?(token, recaptcha_action)
+    secret_key = Rails.application.credentials.dig(:recaptcha_secret_key)
+
+    uri = URI.parse("https://www.google.com/recaptcha/api/siteverify?secret=#{secret_key}&response=#{token}")
+    response = Net::HTTP.get_response(uri)
+    json = JSON.parse(response.body)
+    json['success'] && json['score'] > RECAPTCHA_MINIMUM_SCORE && json['action'] == recaptcha_action
+  end
 end
