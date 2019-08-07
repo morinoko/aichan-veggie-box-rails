ea49e02c7571b39248a57f9b866ffccb21e49e55
Felice Forby <forby.1@gmail.com>
2019-08-08 06:52:29 +0900

    add test for localized methods

--- test/models/veggie_box_test.rb
+++ test/models/veggie_box_test.rb
@@ -20,6 +20,19 @@ class VeggieBoxTest < ActiveSupport::TestCase
   	assert_not @box.valid?
   end
 
+  test "should respond correctly to localized methods" do
+    assert_respond_to @box, :title
+    assert_respond_to @box, :description
+
+    I18n.locale = :ja
+    assert_equal @box.title_ja, @box.title
+    assert_equal @box.description_ja, @box.description
+
+    I18n.locale = :en
+    assert_equal @box.title_en, @box.title
+    assert_equal @box.description_en, @box.description
+  end
+
   test "should not be more than one veggie box" do
   	box_2 = VeggieBox.new(title_ja: "Box 2", description_ja: "invalid box",
                           title_en: "Box 2", description_en: "invalid box")
