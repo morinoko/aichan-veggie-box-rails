b29b1f6e0fea6279e8bc1d2bf7b9d22d6a5b117f
Felice Forby <forby.1@gmail.com>
2019-08-07 09:34:01 +0900

    refactor localized VeggieBox methods using method_missing

--- app/models/veggie_box.rb
+++ app/models/veggie_box.rb
@@ -6,12 +6,14 @@ class VeggieBox < ApplicationRecord
 	validate :only_one
 	has_one_attached :photo
 
-	def localized_title
-		self.send("title_#{I18n.locale}")
-	end
-
-	def localized_description
-		self.send("description_#{I18n.locale}")
+	# Allows for dynamically determined localized title and description based on locale
+	def method_missing(method, *args)
+		if method == "description" || "title"
+			localized_method = "#{method}_#{I18n.locale}"
+			self.send(localized_method)
+		else
+			super
+		end
 	end
 
 	def thumbnail
