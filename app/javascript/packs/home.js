e3b48ab016cec486e27cad25eff978cfb09d105c
Felice Forby <forby.1@gmail.com>
2020-05-29 11:17:37 +0900

    smooth scroll with CSS and stub out home.js

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,25 +1,3 @@
-$(function() {
-	const aboutLink = $("#about-link");
-	aboutLink.click(function(e) {
-		e.preventDefault();
-		$([document.documentElement, document.body]).animate({
-        scrollTop: $("#about").offset().top
-    }, 500);
-	});
-
-	const veggiesLink = $("#veggies-link");
-	veggiesLink.click(function(e) {
-		e.preventDefault();
-		$([document.documentElement, document.body]).animate({
-        scrollTop: $("#veggies").offset().top
-    }, 500);
-	});
-
-	const orderLink = $("#order-link");
-	orderLink.click(function(e) {
-		e.preventDefault();
-		$([document.documentElement, document.body]).animate({
-        scrollTop: $("#order").offset().top
-    }, 500);
-	});
-});
+document.addEventListener("turbolinks:load", function() {
+	// Add any js here
+})
